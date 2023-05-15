a555fbdcbf3790b852cd5bc99fd2f73b54fc21b2
Artem1312 <48861960+Artem1312@users.noreply.github.com>
2023-05-15 20:11:37 +0300

    test_1_2

--- test_1.py
+++ test_1.py
@@ -2,8 +2,10 @@ from kivy.app import App
 from kivy.uix.bubble import Button
 from kivy.uix.codeinput import CodeInput
 from pygments.lexers import HtmlLexer
+
 # https://kivy.org/doc/stable/gettingstarted/installation.html#install-pip
 # https://kivy.org/doc/stable/api-index.html
+# https://www.youtube.com/watch?v=VIy3hktYKwE
 
 from kivy.config import Config
 Config.set('graphics', 'resizable', '0');
@@ -12,21 +14,25 @@ Config.set('graphics', 'height', '480');
 
 
 from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.scatter import Scatter
 
 
 class MyApp(App):
     def build(self):
         # return CodeInput(lexer=HtmlLexer())
+        s = Scatter()
         fl = FloatLayout(size=(300,300))
+        s.add_widget(fl)
         fl.add_widget(Button(text='My first button',
                              font_size=30,
                              on_press=self.btn_press,
                              background_color=[1, 0, 0, 1],
                              background_normal='',
                              size_hint=(.5, .25),
-                             pos=(640/2/2, 480/2-(480*.25/2))));
+                             pos=(640 / 2 / 2, 480 / 2 - (480 * .25 / 2))));
 
-        return fl
+        #return fl
+        return s
 
     def btn_press(self, instance):
         print("Button on press")
