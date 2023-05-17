4e592fb2e63b7244600302fc7cc6373bb5124591
Artem1312 <48861960+Artem1312@users.noreply.github.com>
2023-05-17 22:07:57 +0300

    test_4_1

--- test_4_1.py
+++ test_4_1.py
@@ -8,8 +8,13 @@ from kivy.graphics import (Color, Ellipse, Rectangle, Line)
 class PainterWidget(Widget):
     def on_touch_down(self, touch):
         with self.canvas:
-            Color(0,1,0,1)
-            Ellipse()
+            Color(0, 1, 0, 1)
+            rad = 30
+            Ellipse(pos=(touch.x - rad/2, touch.y - rad/2), size=(rad, rad))
+            touch.ud['line'] = Line(points=(touch.x, touch.y), width=15)
+
+    def on_touch_move(self, touch):
+        touch.ud['line'].points += (touch.x, touch.y)
 
 
 class PaintApp(App):
